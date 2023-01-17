4958e1d4661270c910967cfd560ccf953e132429
sofia <sofiisfernandes@gmaill.com>
2023-01-17 08:18:48 -0300

    Can paint in black by hovering on the grid

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 "use strict";
 
 const container = document.querySelector(".container");
+let mouseDown = false;
 
 for (let i = 0; i < 256; i++) {
   const div = document.createElement("div");
   container.appendChild(div);
 }
+
+document.addEventListener("mousedown", () => (mouseDown = true));
+document.addEventListener("mouseup", () => (mouseDown = false));
+
+container.addEventListener("mousemove", (e) => {
+  if (mouseDown) e.target.style.backgroundColor = "black";
+});
